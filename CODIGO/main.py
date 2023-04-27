01297981730b144af0555d9ffedef417a8f79c7e
@alxgarci <alejandrogarci2000@gmail.com>
2023-04-27 11:31:39 +0200

    +main.py nueva version

--- CODIGO/main.py
+++ CODIGO/main.py
@@ -22,97 +22,79 @@ def main():
     print(">Filas totales de datos: " + str(len(diabetes.index)))
     print(diabetes.to_string())
 
-    #######################################
-    #          PREVISUALIZACION           #
-    #######################################
+    # Importamos nuestros datos desde el csv
+    diabetes_original = p.read_csv("diabetes.csv")
 
-    # Media, Mediana, Moda, Rango, Desviacion Tipica, Varianza
-    # p.Series(diabetes["Pregnancies"].values.flatten()).mode()[0]
-    datos_dict = {
-        "Pregnancies": [diabetes["Pregnancies"].mean(), diabetes["Pregnancies"].median(),
-                        diabetes["Pregnancies"].mode()[0],
-                        diabetes["Pregnancies"].max() - diabetes["Pregnancies"].min(), diabetes["Pregnancies"].std(),
-                        diabetes["Pregnancies"].var()],
-        "Glucose": [diabetes["Glucose"].mean(), diabetes["Glucose"].median(),
-                    diabetes["Glucose"].mode()[0],
-                    diabetes["Glucose"].max() - diabetes["Glucose"].min(), diabetes["Glucose"].std(),
-                    diabetes["Glucose"].var()],
-        "BloodPressure": [diabetes["BloodPressure"].mean(), diabetes["BloodPressure"].median(),
-                          diabetes["BloodPressure"].mode()[0],
-                          diabetes["BloodPressure"].max() - diabetes["BloodPressure"].min(),
-                          diabetes["BloodPressure"].std(),
-                          diabetes["BloodPressure"].var()],
-        "SkinThickness": [diabetes["SkinThickness"].mean(), diabetes["SkinThickness"].median(),
-                          diabetes["SkinThickness"].mode()[0],
-                          diabetes["SkinThickness"].max() - diabetes["SkinThickness"].min(),
-                          diabetes["SkinThickness"].std(),
-                          diabetes["SkinThickness"].var()],
-        "Insulin": [diabetes["Insulin"].mean(), diabetes["Insulin"].median(),
-                    diabetes["Insulin"].mode()[0],
-                    diabetes["Insulin"].max() - diabetes["Insulin"].min(), diabetes["Insulin"].std(),
-                    diabetes["Insulin"].var()],
-        "BMI": [diabetes["BMI"].mean(), diabetes["BMI"].median(),
-                diabetes["BMI"].mode()[0],
-                diabetes["BMI"].max() - diabetes["BMI"].min(), diabetes["BMI"].std(),
-                diabetes["BMI"].var()],
-        "DiabetesPedigreeFunction": [diabetes["DiabetesPedigreeFunction"].mean(),
-                                     diabetes["DiabetesPedigreeFunction"].median(),
-                                     diabetes["DiabetesPedigreeFunction"].mode()[0],
-                                     diabetes["DiabetesPedigreeFunction"].max() - diabetes[
-                                         "DiabetesPedigreeFunction"].min(), diabetes["DiabetesPedigreeFunction"].std(),
-                                     diabetes["DiabetesPedigreeFunction"].var()],
-        "Age": [diabetes["Age"].mean(), diabetes["Age"].median(),
-                diabetes["Age"].mode()[0],
-                diabetes["Age"].max() - diabetes["Age"].min(), diabetes["Age"].std(),
-                diabetes["Age"].var()],
-        "Outcome": [diabetes["Outcome"].mean(), diabetes["Outcome"].median(),
-                    diabetes["Outcome"].mode()[0],
-                    diabetes["Outcome"].max() - diabetes["Outcome"].min(), diabetes["Outcome"].std(),
-                    diabetes["Outcome"].var()]
+    # Quitamos la columna "Outcome" ya que no nos servirá para nuestro estudio estadístico
+    diabetes = diabetes_original.drop("Outcome", axis="columns")
+    print(diabetes.to_string())
 
-    }
+    # Calculamos la Media, Mediana, Moda, Rango, Desviacion Tipica, Varianza de cada variable y la metemos en un
+    # diccionario que mas tarde representaremos en una tabla
+    datos_dict = dict()
+    for x in diabetes.columns:
+        datos_dict.update({
+            x: [diabetes[x].mean(), diabetes[x].median(),
+                diabetes[x].mode()[0],
+                diabetes[x].max() - diabetes[x].min(), diabetes[x].std(),
+                diabetes[x].var()]
+        })
 
-    p.options.display.float_format = '{:,.2f}'.format
     columnas = ["Media", "Mediana", "Moda", "Rango", "Desviacion Tipica", "Varianza"]
-    media_moda_etc = p.DataFrame.from_dict(datos_dict, orient="index",
-                                           columns=columnas)
-
+    media_moda_etc = p.DataFrame.from_dict(datos_dict, orient="index", columns=columnas)
     print(media_moda_etc.to_string())
 
-    media_moda_etc.loc[media_moda_etc.index.drop(["Insulin", "Glucose"])].plot.bar(rot=0)
-
-    plot.title("Media, moda, etc de todas las variables")
+    # Graficas de barras de la Media, Mediana, Moda, Rango, Desviacion Tipica de cada variable
+    varianza = media_moda_etc["Varianza"]
+    media_moda_etc2 = media_moda_etc.drop(["Varianza"], axis="columns")
+    media_moda_etc2.loc[media_moda_etc2.index.drop(["Insulin", "Glucose", "BloodPressure"])].plot.bar(rot=0)
+    plot.xticks(rotation=45)
+    plot.show()
+    media_moda_etc2.loc[["Insulin", "Glucose", "BloodPressure"]].plot.bar(rot=0)
+    plot.xticks(rotation=45)
     plot.show()
 
-    media_moda_etc.loc[["Insulin", "Glucose"]].plot.bar(rot=0)
-
-    plot.title("Media, moda, etc de Insulin y Glucose")
+    # Varianza de cada variable
+    plot.figure()
+    varianza.plot.bar(rot=0)
+    # Etiquetas rotadas para mejor lectura
+    plot.xticks(rotation=45)
     plot.show()
 
-    # Histograma
+    # Histogramas de cada variable
     for x in diabetes.columns:
+        plot.figure()
         sb.histplot(diabetes[x].T)
         plot.title("Histograma de " + x)
         plot.show()
 
-    # Diagrama de cajas
-    columnas_diabetes = list(diabetes)
-    print(columnas_diabetes)
-    sb.boxplot(diabetes[diabetes.columns.drop(["Insulin", "Glucose"])])
+    # Diagrama de cajas de cada variable
+    sb.boxplot(diabetes[diabetes.columns.drop(["Insulin", "Glucose", "BloodPressure"])])
     plot.title("Diagrama de cajas (1/2)")
+    plot.xticks(rotation=45)
     plot.show()
-    #
-    # sb.boxplot(diabetes[["Insulin", "Glucose"]])
-    # plot.title("Diagrama de cajas (2/2)")
-    # plot.show()
 
+    plot.figure()
+    sb.boxplot(diabetes[["Insulin", "Glucose", "BloodPressure"]])
+    plot.title("Diagrama de cajas (2/2)")
+    plot.xticks(rotation=45)
+    plot.show()
 
-    # Pruebas
-    # for x in media_moda_etc.index:
-    #     # sb.barplot(x=media_moda_etc.columns, data=media_moda_etc.loc[x])
-    #     media_moda_etc.loc[x].plot.bar(rot=0)
-    #     plot.title("Media, moda, etc de " + x)
-    #     plot.show()
+    # Creamos una matriz de correlacion para poder ver cuales son
+    # las variables más relacionadas de forma clara por medio de su coeficiente de correlacion
+    sb.heatmap(diabetes.corr())
+    plot.title("Matriz de coeficiente de correlación entre variables")
+    plot.show()
+
+    # Diagrama de correlacion por pares, para entender la anterior matriz de correlacion
+    sb.pairplot(diabetes)
+    plot.show()
+
+    # TODO:
+    # ✓Un análisis de regresión para las dos variables continuas, así como su gráfico de dispersión y el
+    # coeficiente de correlación.
+    # ✓Gráficos y tablas que pueden explicar las variables de una manera más completa (diagramas de
+    # caja según variable categórica, por ejemplo)
 
 
 if __name__ == '__main__':
